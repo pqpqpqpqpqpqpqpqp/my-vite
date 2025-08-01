a3791d131b25c6bd320a097db50fc0c65e12452b
HDH <hong135689@gmail.com>
2025-08-01 20:28:29 +0900

    feat: Add initial setup for Google Maps API

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { HiMenu } from "react-icons/hi";
+
+function Header() {
+    return (
+        <header className="px-10 py-7 flex justify-between">
+            <div className="font-bold">My Application</div>
+            <HiMenu className="text-2xl cursor-pointer" />
+        </header>
+    );
+}
+
+export default Header;
